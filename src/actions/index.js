86e88dbc1cd4b53e86a0f00c9e5eddc0d493e959
Kelsey Durmeier <KelseyDurmeier09@gmail.com>
2021-10-31 14:39:02 -0500

    added SET_RESULTS_POSTS reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { SET_RECENT_POSTS } from './types';
+import { 
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
+} from './types';
 
 import axios from "axios"
 
@@ -8,7 +11,7 @@ export function fetchRecentPosts() {
         .then(response => {
             console.log(response.data.posts);
             dispatch({
-                type: SET_RECENT_POSTS,
+                type: SET_RESULTS_POSTS,
                 payload: response.data.posts
             });
         })
